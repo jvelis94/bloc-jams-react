2fb097adcc32cbfae46fc27c4eaaed29cb812d96
Joel Velis <joelvelis94@gmail.com>
2019-01-17 11:30:25 -0500

    assignment-router complete

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+	render() {
+		return (
+			<section className="album">
+				ALbum page will go here
+			</section>
+);
+}
+}
+
+export default Album;
