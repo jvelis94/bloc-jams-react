699dcdd0fd1ba691497f9e60c5849a77dcde24cd
Joel Velis <joelvelis94@gmail.com>
2019-01-22 16:54:11 -0500

    assignment-album complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,9 @@ class Album extends Component {
      });
  
      this.state = {
-       album: album
-     };	
+       album: album,
+       
+    };	
 
 		}
 	
@@ -34,6 +35,14 @@ class Album extends Component {
             		 	<col id="song-duration-column" />
            			</colgroup>  
           			<tbody>
+          				{ this.state.album.songs.map( (song, index) =>
+							<tr key={index} >
+								<td>{index+1}</td>
+								<td>{song.title}</td>
+								<td>{song.duration}</td>
+                    		</tr>
+						)}
+							
            			</tbody>
          			</table>
 			</section>
